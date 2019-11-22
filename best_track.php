9a0a81482a6fd4857e6ca483714c824d71689729
John Yeung <45678191+johnyhh@users.noreply.github.com>
2019-11-22 12:20:24 +0800

    Update best_track.php

--- best_track.php
+++ best_track.php
@@ -1,12 +1,17 @@
 <?php
-print "test";
+$start = 0;
+$content = "";
+$lines = array();
+$arr   = array();
+$datetime = "";
+$lat = 0;
+$long = 0;
+$pw = 0;
+$yrmn = "";
 
 $url="http://www.typhoon2000.ph/multi/log.php?name=FUNG-WONG_2019";
 $page = file_get_contents($url);
 $lines = explode("\n",$page);
-
-$start = 0;
-$content = "";
 
 for ($i=0;$i<=count($lines);$i++){
 	if (preg_match("/function preload/", $lines[$i])){
@@ -14,12 +19,19 @@ for ($i=0;$i<=count($lines);$i++){
 	} elseif (preg_match("/body onLoad/", $lines[$i])){
 		$start = 0;
 	}
+	
 	if ($start == 1)
 	{
-		if (preg_match("/text/", $lines[$i])){
+		if (preg_match("/text\[/", $lines[$i])){
 			// each line[$i] here is data
 			// time: (2019-11-07 09:31:53 UTC)
+		    if (preg_match('/([0-9]{4})-([0-9]{2})-([0-9]{2}) /', $lines[$i], $arr)){
+				$yrmn = "$arr[1]$arr[2]$arr[3]";
+			}			
 			// HKO:\n041800Z 18.7N 152.3E 105KT\n => *1.1
+		    if (preg_match('/HKO\:....(..)..Z ([0-9]+.[0-9])N ([0-9]+.[0-9])E ([0-9]+)KT/', $lines[$i], $arr)){
+				$datetime = $yrmn.$arr[1]; $lat = $arr[2]; $long = $arr[3]; $pw = floor($arr[4] * 1.1);
+			}
 			// JTWC:\n041800Z 18.7N 152.3E 105KT\n
 			// JMA:\n041800Z 18.8N 152.3E 80KT\n => *1.1
 			// NMC:\n041800Z 18.9N 152.3E 87KT\n
@@ -33,6 +45,7 @@ for ($i=0;$i<=count($lines);$i++){
 			// Output text file
 			
 			$content .= $lines[$i] . "<\n>";
+			print "時間: $datetime , 北緯: $lat 東經: $long 強度 $pw kts <br>";
 		}
 	}
 }
