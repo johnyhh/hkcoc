5be7e096e3eafee3703cbe733bf6e0f8e6a1f02e
John Yeung <45678191+johnyhh@users.noreply.github.com>
2020-08-06 10:16:20 +0800

    v0.2
    	// Flow
    	// 1. Call by wi cgi / cronjob
    	// 2. check if there are n row, delete first row (keep last 7 days record)
    	// 3. get and format wi.txt (single row)   [OK]
    	// 4. check if they are same with last row, skip
    	// 5. else, append to last line  [OK]
    	
    	// Todo order
    	// 4,2,1

--- wi_sync_gs.php
+++ wi_sync_gs.php
@@ -1,4 +1,13 @@
 <?php
+	// Flow
+	// 1. Call by wi cgi / cronjob
+	// 2. check if there are n row, delete first row (keep last 7 days record)
+	// 3. get and format wi.txt (single row)   [OK]
+	// 4. check if they are same with last row, skip
+	// 5. else, append to last line  [OK]
+	
+	// Todo order
+	// 4,2,1
 
 	// Loading google api library
 	require_once 'google-api-php-client-v2.7.0-PHP5.4/vendor/autoload.php';
@@ -25,29 +34,40 @@
 	{		
 		// Find last row having data
 		$lastrow = sizeof($rows);
-		print $rows[$lastrow-1][0] . " | " . $rows[$lastrow-1][1];		
+		print "Last row : " . $rows[$lastrow-1][0] . " | " . $rows[$lastrow-1][1];		
 
-		// print_r($data);		
+		// print_r($data);	
+
+		list($datetime, $wi) = getwi();
+		print "to be write: ".$datetime." ".$wi."<br>";
 		
 		// Append values to last row of range
 		$range = "A1:B";
 		$valueRange= new \Google_Service_Sheets_ValueRange();
-		$valueRange->setValues(["values" => ["a", "b"]]); 
+		$valueRange->setValues(["values" => [$datetime, $wi]]); 
 		$conf = ["valueInputOption" => "RAW"];
 		$ins = ["insertDataOption" => "INSERT_ROWS"];
 		$sheets->spreadsheets_values->append($spreadsheetId, $range, $valueRange, $conf, $ins);
 	}
 	
-	// Flow
-	// 1. Call by wi cgi / cronjob
-	// 2. check if there are n row, delete first row (keep last 7 days record)
-	// 3. get and format wi.txt (single row)
-	// 4. check if they are same with last row, skip
-	// 5. else, append to last line
-	
-	// Todo order
-	// 3,5,4,2,1
-	
+function getwi()
+{
+	// get and format last record at wi.txt
+	$url = "http://www.hkcoc.com/wind/test/newwi.txt";
 
+	// Get TC data from internet
+	$page = file_get_contents($url);
+	$lines = explode("\n",$page);
+	$data = explode(" ",$lines[sizeof($lines)-2]);
 	
+	if (preg_match('/([0-9]{4})([0-9]{2})([0-9]{2})/' , $data[0], $yyyymmdd))
+	{
+		if (preg_match('/([0-9]{2})([0-9]{2})/' , $data[1], $hhmm))
+		{
+			$wi = $data[2];
+			$datetime = $yyyymmdd[1]."-".$yyyymmdd[2]."-".$yyyymmdd[3]." ".$hhmm[1].":".$hhmm[2].":00";
+		}
+	}
+	return array ($datetime, $wi);
+}	
 ?>	
